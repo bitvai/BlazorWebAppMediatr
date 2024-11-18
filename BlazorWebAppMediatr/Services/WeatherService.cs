23cd5534f338f1c48a9b90fb77b71e3e9c95c5de
Bitvai Csaba <csaba.bitvai@semilab.hu>
2024-11-18 15:25:36 +0100

    Add initial request, example

--- BlazorWebAppMediatr/Services/WeatherService.cs
+++ BlazorWebAppMediatr/Services/WeatherService.cs
@@ -32,8 +32,36 @@ namespace BlazorWebAppMediatr.Services
         }
     }
 
+    public class WeatherRequestHandler : IRequestHandler<WeatherRequest, WeatherResponse>
+    {
+        public Task<WeatherResponse> Handle(WeatherRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<WeatherResponse>(new WeatherResponse() { Forecasts = WeatherForecasts().ToList() });
+        }
+
+        private static WeatherForecast[] WeatherForecasts()
+        {
+
+            var startDate = DateOnly.FromDateTime(DateTime.Now);
+            var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast()
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            }).ToArray();
+            return forecasts;
+        }
+    }
+
     public record WeatherStreamQuery : IStreamRequest<WeatherStreamQueryResult>;
     public record WeatherStreamQueryResult
+    {
+        public List<WeatherForecast> Forecasts { get; set; }
+    }
+
+    public record WeatherRequest : IRequest<WeatherResponse>;
+    public record WeatherResponse
     {
         public List<WeatherForecast> Forecasts { get; set; }
     }
